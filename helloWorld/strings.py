e3fc345f9fd6ae73a55e4e4fd0a88d41e9c8ab53
Ladislav Vašina <ladislavvasina@gmail.com>
2021-07-13 21:45:47 +0200

    Prints

---
+++ helloWorld/strings.py
@@ -0,0 +1,12 @@
+print("Today is a good day to learn python")
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in string')
+print("Hello" + " world")
+
+greeting = "Hello"
+name = input("Please enter your name: ")
+
+print(greeting + ' ' + name)
+
+
