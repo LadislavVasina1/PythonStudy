dad10fd6401429837d198ce2fe0ed26d1514749a
Ladislav Vašina <ladislavvasina@gmail.com>
2021-08-06 12:27:10 +0200

    Slicing by steps and separating by multiple separators

--- helloWorld/strings2.py
+++ helloWorld/strings2.py
@@ -37,4 +37,20 @@ print(parrot[:])
 print(parrot[-4:-2])
 print(parrot[-4:12])
 
+# SLICING BY STEPS
+print(parrot[0:6:2])  # Nre - Slice stars at index position 0 - It extends up to position 6 - We step through the sequence in steps of 2
+print(parrot[0:6:3])  # Nw
+
+number = "9,223;372:036 854,775;807"
+separators = number[1::4]  # From index 1 (this index including) get every fourth char
+print(separators)
+
+# PRINTING VALUES SEPARATED BY MULTIPLE SEPARATORS
+values = "".join(char if char not in separators else " " for char in number).split()
+print(values)
+print([int(val) for val in values])
+
+
+
+
 
