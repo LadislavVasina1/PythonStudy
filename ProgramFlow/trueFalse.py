fe60ae7a1aa24680a7229eca5c24fd830b23124d
Ladislav Vašina <ladislavvasina@gmail.com>
2021-08-10 09:56:15 +0200

    TrueFalse

---
+++ ProgramFlow/trueFalse.py
@@ -0,0 +1,15 @@
+day = "Monday"
+temperature = 30
+raining = True
+
+if (day == "Saturday" and temperature > 27) or not raining:
+    print("Go swimming.")
+else:
+    print("Learn Python")
+
+name = input("Enter your name: ")
+if name:
+    print(f"Hi, {name}")
+else:
+    print("Are you the man with no name?")
+
