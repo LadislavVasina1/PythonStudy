73e5d16940d5c68f5da37d8a30d52ad377cb9471
Ladislav Vašina <ladislavvasina@gmail.com>
2021-08-10 09:33:27 +0200

    Guessing game

--- ProgramFlow/guessingGame.py
+++ ProgramFlow/guessingGame.py
@@ -5,21 +5,36 @@ answer = random.randint(1, 10)
 print("Please guess a number between 1 an 10: ")
 guess = int(input())
 
-if guess < answer:
-    print("Please guess higher")
+if guess != answer:
+    if guess < answer:
+        print("Please guess higher.")
+    else:
+        print("Please guess lower.")
     guess = int(input())
     if guess == answer:
-        print("Well done, you guessed it")
+        print("Good job, you have guessed it.")
     else:
-        print("You have not guessed it correctly")
-        print(f"Correct answer is: {answer}")
-elif guess > answer:
-    print("Please guess lower")
-    guess = int(input())
-    if guess == answer:
-        print("Well done, you guessed it")
-    else:
-        print("You have not guessed it correctly")
-        print(f"Correct answer is: {answer}")
+        print("Sorry, you have not guessed it correctly.")
 else:
-    print("You have guess the right number")
+    print("You got it first time.")
+
+# if guess < answer:
+#     print("Please guess higher")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("You have not guessed it correctly")
+#         print(f"Correct answer is: {answer}")
+# elif guess > answer:
+#     print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you guessed it")
+#     else:
+#         print("You have not guessed it correctly")
+#         print(f"Correct answer is: {answer}")
+# else:
+#     print("You have guess the right number")
+
+
