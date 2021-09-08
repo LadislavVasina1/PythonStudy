af00c7157b00f2a273812a1d95f5be45778eee2c
Ladislav Vašina <ladislavvasina@gmail.com>
2021-09-08 13:17:07 +0200

    Using splits and simpler adder

--- Sequences/spliting_things.py
+++ Sequences/spliting_things.py
@@ -7,3 +7,10 @@ print(words)
 
 numbers = "9,223,372,036,854,775,804"
 print(numbers.split(","))
+
+seq = input()
+separated = seq.split(",")
+result = 0
+for number in separated:
+    result += int(number)
+print(result)
